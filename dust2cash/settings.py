f62e0cea9766c67178415293d2404e863c935242
Michael Ng'ang'a <michaelnganga678@gmail.com>
2025-11-17 09:05:58 +0300

    Update static and media file settings
    Configure static and media file settings for production.

--- dust2cash/settings.py
+++ dust2cash/settings.py
@@ -68,7 +68,14 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
+
+
 
 # Required for production (Render, Heroku, etc.)
 STATIC_ROOT = BASE_DIR / 'staticfiles'
